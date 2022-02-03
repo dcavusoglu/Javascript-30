f9b3bfa20c15ca00ade3d1fa002ab0d0d2f66f42
Duygu Cavusoglu <cvsgl.dyg@gmail.com>
2022-02-03 16:41:15 +0300

    completed

--- 13-Slide-in-on-Scroll/script.js
+++ 13-Slide-in-on-Scroll/script.js
@@ -12,3 +12,31 @@ function debounce(func, wait = 20, immediate = true) {
         if (callNow) func.apply(context, args);
       };
     }
+
+    const sliderImages = document.querySelectorAll('.slide-in');
+
+    function slideUpdate(e) {
+      sliderImages.forEach(image => {
+        const slideInAt = (window.scrollY + window.innerHeight)
+        - image.height / 2;
+        console.log(slideInAt);
+
+        const imageBottom = image.offsetTop + image.height;
+
+        //console.log('image bottom:', imageBottom);
+
+        const imageHalfShown = slideInAt > image.offsetTop;
+        //console.log('half:', imageHalfShown);
+
+        const imageNotPassed = window.scrollY < imageBottom;
+        //console.log('not passed:', imageNotPassed);
+        if(imageHalfShown && imageNotPassed) {
+          image.classList.add('active');
+        }
+        else {
+          image.classList.remove('active');
+        }
+      });
+    }
+
+    window.addEventListener('scroll', slideUpdate);
