7c82361e055355b4c3f10599911050b3c6c58fef
Duygu Cavusoglu <cvsgl.dyg@gmail.com>
2022-02-06 11:39:51 +0300

    done

--- 17-Sorting-Band-Names-without-Articles/script.js
+++ 17-Sorting-Band-Names-without-Articles/script.js
@@ -1,1 +1,16 @@
 const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+const bandList = document.querySelector('#bands');
+
+function noArticles(band) {
+  return band.replace(/^(the |a |an )/i, '').trim();
+}
+
+
+const orderBands = bands.sort((a,b) => noArticles(a) > noArticles(b) ? 1 : -1);
+
+bandList.innerHTML = orderBands.map(band =>
+  `<li>${band}</li>`
+).join('');
+
+
+console.log(orderBands);
